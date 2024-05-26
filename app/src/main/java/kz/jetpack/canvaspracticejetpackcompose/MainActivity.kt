1813da147a5e6fba0655c68349cb70122736bd59
Nursultan <nursamanzhol@gmail.com>
2024-05-26 14:47:08 +0500

    start practice #2 Android Studio Tutorial

--- app/src/main/java/kz/jetpack/canvaspracticejetpackcompose/MainActivity.kt
+++ app/src/main/java/kz/jetpack/canvaspracticejetpackcompose/MainActivity.kt
@@ -3,20 +3,28 @@ package kz.jetpack.canvaspracticejetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kz.jetpack.canvaspracticejetpackcompose.presentation.CanvasPractice
 import kz.jetpack.canvaspracticejetpackcompose.ui.theme.CanvasPracticeJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            CanvasPractice()
         }
     }
 }
+
